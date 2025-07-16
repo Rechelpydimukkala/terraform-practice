514652e24a7f2c83ca048f977afd5f457b29ed2a
Rechelpydimukkala <143074892+Rechelpydimukkala@users.noreply.github.com>
2025-07-16 14:48:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0f88e80871fd81e91"
+    ami = "ami-0a1235697f4afa8a4"
     instance_type = "t2.nano"
     tags = {
       Name = "devvr"
