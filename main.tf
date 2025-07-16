56da8ab28bbb02c34826dd8ce88766457703171a
Rechelpydimukkala <143074892+Rechelpydimukkala@users.noreply.github.com>
2025-07-16 15:12:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "dev" {
     ami = "ami-0150ccaf51ab55a51"
-    instance_type = "t2.nano"
+    instance_type = "t2.micro"
     tags = {
       Name = "devvr"
     }
